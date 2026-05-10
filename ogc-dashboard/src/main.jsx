46875a1dd62bd2d66d7ae0e7c8ff67ed17432398
jesus-justin <mjesusjustin@gmail.com>
2026-05-10 08:36:10 +0800

    fix: Apply HashRouter to OGC dashboard as well
    - Change OGC dashboard from BrowserRouter to HashRouter for consistency
    - Ensures all facilitator navigation works correctly with hash-based URLs

--- ogc-dashboard/src/main.jsx
+++ ogc-dashboard/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { FacilitatorAuthProvider } from './context/FacilitatorAuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <FacilitatorAuthProvider>
         <App />
       </FacilitatorAuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
